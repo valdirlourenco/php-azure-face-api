3b30c7854c5b0853cf9510d6cd9991cf34b4573e
Valdir Lourenco <valdir2310@yahoo.com.br>
2019-08-28 09:42:51 -0300

    created the regions

---
+++ src/Regions.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SmartDog23\AzureFaceApi;
+
+class Regions {
+
+    const AUSTRALIA_EAST = 'australiaeast';
+    const BRAZIL_SOUTH = 'brazilsouth';
+    const CANADA_CENTRAL = 'canadacentral';
+    const CENTRAL_INDIA = 'centralindia';
+    const CENTRAL_US = 'centralindia';
+
+    const EAST_ASIA = 'eastasia';
+    const EAST_US = 'eastus';
+    const EAST_US_2 = 'eastus2';
+    const FRANCE_CENTRAL = 'francecentral';
+    const JAPAN_EAST = 'japaneast';
+
+    const JAPAN_WEST = 'japanwest';
+    const KOREA_CENTRAL = 'koreacentral';
+    const NORTH_CENTRAL_US = 'northcentralus';
+    const NORTH_EUROPE = 'northeurope';
+    const SOUTH_AFRICA_NORTH = 'southafricanorth';
+
+    const SOUTH_CENTRAL_US = 'southcentralus';
+    const SOUTHEAST_ASIA = 'southeastasia';
+    const UK_SOUTH = 'uksouth';
+    const WEST_CENTRAL_US = 'westcentralus';
+    const WEST_EUROPE = 'westeurope';
+
+    const WEST_US = 'westus';
+    const WEST_US_2 = 'westus2';
+
+}

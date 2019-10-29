4e4a91b9acc83b3be92490215033aace6f4dbbe8
Valdir Lourenco <valdir2310@yahoo.com.br>
2019-10-29 17:51:28 -0200

    facehelper azureapi in constructor

--- src/FaceHelper.php
+++ src/FaceHelper.php
@@ -9,50 +9,53 @@ use SmartDog23\AzureFaceApi\LargePersonGroupPerson\AddFace\AddFaceOptions;
 
 class FaceHelper {
 
-    public function __construct()
+    private $azureFaceApi;
+
+    public function __construct(AzureFaceApi $azureFaceApi)
     {
+        $this->azureFaceApi = $azureFaceApi;
     }
 
     /**
      * @param AzureFaceApi $azureFaceApi
      */
-    public function createLargePersonGroup($azureFaceApi, $groupId)
+    public function createLargePersonGroup($groupId)
     {
         $options = new CreateOptions();
         $options->parameters()->largePersonGroupId($groupId);
         $options->body()->name('Description for '.$groupId);
         $options->body()->userData('User data for the group: '.$groupId);
-        return $azureFaceApi->largePersonGroup()->create($options);
+        return $this->azureFaceApi->largePersonGroup()->create($options);
     }
 
-    public function createLargePersonGroupPerson(AzureFaceApi $azureFaceApi, $groupId)
+    public function createLargePersonGroupPerson($groupId)
     {
         $options = new \SmartDog23\AzureFaceApi\LargePersonGroupPerson\Create\CreateOptions();
         $options->parameters()->largePersonGroupId($groupId);
         $name = 'name '.rand(1000, 9999);
         $options->body()->name($name);
         $options->body()->userData('User data for person: '.$name);
-        return $azureFaceApi->largePersonGroupPerson()->create($options);
+        return $this->azureFaceApi->largePersonGroupPerson()->create($options);
     }
 
-    public function addFaceToPersonInGroup(AzureFaceApi $azureFaceApi, $groupId, $responsePerson)
+    public function addFaceToPersonInGroup($groupId, $responsePerson)
     {
         $options = new AddFaceOptions();
         $options->parameters()->largePersonGroupId($groupId);
         $options->parameters()->personId($responsePerson);
         $options->parameters()->userData('User data for person in group: ');
         $options->body()->url('https://essencialpromo.com/test/hollywood/ss-001.jpg');
-        return $azureFaceApi->largePersonGroupPerson()->addFace($options);
+        return $this->azureFaceApi->largePersonGroupPerson()->addFace($options);
     }
 
     /**
      * @param AzureFaceApi $azureFaceApi
      */
-    public function trainGroup($azureFaceApi, $groupId)
+    public function trainGroup($groupId)
     {
         $options = new TrainOptions();
         $options->parameters()->largePersonGroupId($groupId);
-        return $azureFaceApi->largePersonGroup()->train($options);
+        return $this->azureFaceApi->largePersonGroup()->train($options);
     }
 
 }

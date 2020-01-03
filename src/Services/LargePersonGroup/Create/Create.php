b4e0a7b951ff00a9593d544530eeb90b782a4889
Valdir Lourenco <valdir2310@yahoo.com.br>
2020-01-03 13:02:40 -0300

    create azure face api service with options method

--- src/Services/LargePersonGroup/Create/Create.php
+++ src/Services/LargePersonGroup/Create/Create.php
@@ -11,22 +11,13 @@ class Create extends AzureFaceApiService
     public function __construct($client)
     {
         parent::__construct($client);
+        $this->_optionsClass = CreateOptions::class;
     }
 
     public function executeWithOptions($options = null)
     {
-        if (is_null($options)) {
-            $options = new CreateOptions();
-        }
-        $this->_options = $options;
-
-        $optionsRequest = [];
-        $optionsRequest['headers'] = $this->_options->headers()->toArray();
-        $optionsRequest['query'] = $this->_options->parameters()->toArray();
-        $optionsRequest['body'] = $this->_options->body()->toJson();
-        $url = 'largepersongroups/' . $this->_options->parameters()->getLargePersonGroupId();
-        $response = $this->_client->request('PUT', $url, $optionsRequest);
-        return $response;
+        $url = 'largepersongroups/' . $options->parameters()->getLargePersonGroupId();
+        return $this->executeWithOptionsCall($options, $url, 'PUT');
     }
 
     public function execute($groupId, $description, $userData = null)
--- src/Utilities/AzureFaceApiService.php
+++ src/Utilities/AzureFaceApiService.php
@@ -6,9 +6,31 @@ class AzureFaceApiService
 {
     /** @var AzureHttpClient */
     protected $_client;
+    protected $_optionsClass;
+    protected $_url;
+    protected $_method;
+
+    private $_options;
 
     public function __construct($client)
     {
         $this->_client = $client;
+    }
+
+    protected function executeWithOptionsCall($options, $url, $method)
+    {
+//        if (is_null($options)) {
+//            $options = new $this->_optionsClass;
+//        }
+
+
+
+        $optionsRequest = [];
+        $optionsRequest['headers'] = $options->headers()->toArray();
+        $optionsRequest['query'] = $options->parameters()->toArray();
+        $optionsRequest['body'] = $options->body()->toJson();
+//        $url = 'largepersongroups/' . $options->parameters()->getLargePersonGroupId();
+        $response = $this->_client->request($method, $url, $optionsRequest);
+        return $response;
     }
 }
